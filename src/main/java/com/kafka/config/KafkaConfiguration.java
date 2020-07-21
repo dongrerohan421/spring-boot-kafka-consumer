94cdc1da3202f1e3e8abaf45891188bf75a9c0e1
dongrerohan421 <dongrerohan421@gmail.com>
2020-07-21 10:00:57 -0400

    consume text message

--- src/main/java/com/kafka/config/KafkaConfiguration.java
+++ src/main/java/com/kafka/config/KafkaConfiguration.java
@@ -4,13 +4,15 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
-import org.apache.kafka.common.serialization.StringSerializer;
+import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 
+@EnableKafka
 @Configuration
 public class KafkaConfiguration {
 
@@ -21,8 +23,8 @@ public class KafkaConfiguration {
 		
 		configs.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
 		configs.put(ConsumerConfig.GROUP_ID_CONFIG, "group_id");
-		configs.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringSerializer.class);
-		configs.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringSerializer.class);
+		configs.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+		configs.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
 		
 		return new DefaultKafkaConsumerFactory<>(configs);
 	}
